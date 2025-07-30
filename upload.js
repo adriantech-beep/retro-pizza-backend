b4dd55515083091b8243ecd06656450de7ba0352
adriantech-beep <Adhrianne29@gmail.com>
2025-07-30 16:21:44 +0800

    fixed deployment error

--- upload.js
+++ upload.js
@@ -1,7 +1,8 @@
 import multer from "multer";
 
 import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "@cloudinary/multer-storage-cloudinary";
+
 import dotenv from "dotenv";
 dotenv.config();
 
