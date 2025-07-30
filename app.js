a16175500f387ff5a8dbf032e66016647f1af174
adriantech-beep <Adhrianne29@gmail.com>
2025-07-30 18:03:37 +0800

    one last fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(import.meta.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
